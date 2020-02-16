fb014c934c9d69cd3ec178d6032e9dbe9fe28a28
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-16 04:53:34 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,17 +5,3 @@
 
 
 
-class EmailAddressParser
-	attr_accessor :emails
-	
-	def initialize(emails)
-		@emails= emails
-	end
-	
-	def parse
-		@emails = @emails.gsub(/[\s,]/ ," ").split
-		
-		@emails.uniq
-	end
-	
-end
