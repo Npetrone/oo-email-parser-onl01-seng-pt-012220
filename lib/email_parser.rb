abc6b4d1cd802532edad815e40d32e8e6412206c
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-16 04:57:17 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,17 +5,18 @@
 
 
 
-class EmailAddressParser 
-  attr_accessor :csv_emails
+#class EmailAddressParser 
+ # attr_accessor :csv_emails
   
-  def initialize(csv_emails)
-    @csv_emails = csv_emails
-  end
+  #def initialize(csv_emails)
+   # @csv_emails = csv_emails
+  #end
 
-  def parse 
-    csv_emails.split.collect do |address|
-      address.split(',') 
-    end
-    .flatten.uniq 
-  end 
-end 
+  #def parse 
+   # csv_emails.split.collect do |address|
+    #  address.split(',') 
+    #end
+    #.flatten.uniq 
+  #end 
+#end 
+
